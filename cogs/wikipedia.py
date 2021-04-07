1ec6bded8bd93e1d9ea6ee357a177f589ce848df
Flavio Irani <54213802+coolfav@users.noreply.github.com>
2021-04-07 22:32:49 +0300

    Remade wikipedia.py
    Re-created the wikipedia script to lift links directly from wikipedia instead of searching google and outputting the top link. Theoretically should not break anything.
    
    Credits for the remake go to Cynthia Dev and Mysttt

--- cogs/wikipedia.py
+++ cogs/wikipedia.py
@@ -1,28 +1,24 @@
 import discord
 import shutil
 import os
-from googlesearch import search
+
 import wikipedia
 from discord.ext import commands
-from discord.utils import get
-from mainbot import client
 
 
 # Created by coolfav on Github
+# Re-created by Cynthia Dev#0001 (VerstreuteSeele on Github) and ryann#7322 (Mysttt on Github)
+
 
 class Wikipedia(commands.Cog):
-
     def __init__(self, client):
         self.client = client
 
     @commands.command(aliases=["wiki", "w"])
     async def wikipedia(self, ctx, *query):
         thequery = " ".join(query)
-        for j in search(thequery, tld="com", num=1, stop=1, pause=2):
-            await ctx.send(j)
-        await ctx.send(wikipedia.summary(wikipedia.search(thequery, results=1, suggestion=False), sentences=3, chars=0,
-                                   auto_suggest=True, redirect=True))
-
+        link = wikipedia.page(thequery)
+        await ctx.send(link.url)
 
 
 def setup(client):
